e7ffef439c067eeb3e048fb2d25e515036260c6b
Jongwan Kim <jongwankim@Jongwans-iMac.local>
2023-01-25 16:45:10 +0900

    updated App.tsx

--- react-webgl-unity/src/App.tsx
+++ react-webgl-unity/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  const { unityProvider, isLoaded, loadingProgression  } = useUnityContext({
+    loaderUrl: "Build/2d-anime-no-zip.loader.js",
+    dataUrl: "Build/2d-anime-no-zip.data",
+    frameworkUrl: "Build/2d-anime-no-zip.framework.js",
+    codeUrl: "Build/2d-anime-no-zip.wasm"
+  });
+
+  const loadingPercentage = Math.round(loadingProgression * 100);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="container">
+        {isLoaded === false && (
+          <div className="loading-overlay">
+            <p>Loading... ({loadingPercentage}%)</p>
+          </div>
+        )}
+        <Unity
+          unityProvider={unityProvider}
+          style={{height: "100vh"}}
+        />
+      </div>
+
   );
 }
 
 export default App;
+
