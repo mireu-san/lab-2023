3fb858c2566914f0c6864ddd65c304735a22b4ce
Jongwan Kim <jongwankim@Jongwans-iMac.local>
2023-01-25 17:24:06 +0900

    disabled unnecessary effect - temp

--- react-webgl-unity/src/App.tsx
+++ react-webgl-unity/src/App.tsx
@@ -3,27 +3,27 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
   const { unityProvider, isLoaded, loadingProgression  } = useUnityContext({
-    loaderUrl: "Build/2d-anime-no-zip.loader.js",
-    dataUrl: "Build/2d-anime-no-zip.data",
-    frameworkUrl: "Build/2d-anime-no-zip.framework.js",
-    codeUrl: "Build/2d-anime-no-zip.wasm"
+    loaderUrl: "../public/Build/2d-anime-no-zip.loader.js",
+    dataUrl: "../public/Build/2d-anime-no-zip.data",
+    frameworkUrl: "../public/Build/2d-anime-no-zip.framework.js",
+    codeUrl: "../public/Build/2d-anime-no-zip.wasm"
   });
 
   const loadingPercentage = Math.round(loadingProgression * 100);
 
   return (
 
-      <div className="container">
-        {isLoaded === false && (
-          <div className="loading-overlay">
-            <p>Loading... ({loadingPercentage}%)</p>
-          </div>
-        )}
+      // <div className="container">
+      //   {isLoaded === false && (
+      //     <div className="loading-overlay">
+      //       <p>Loading... ({loadingPercentage}%)</p>
+      //     </div>
+      //   )}
         <Unity
           unityProvider={unityProvider}
           style={{height: "100vh"}}
         />
-      </div>
+      // </div>
 
   );
 }
